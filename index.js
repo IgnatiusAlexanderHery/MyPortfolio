81506611018bd3fbfe907bf2e94110644230052c
IgnatiusAlexanderHery <alexander310702@gmail.com>
2023-05-01 12:59:38 +0700

    Update Dark mode and fix bugs

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 const navItems = document.querySelectorAll(".nav-item");
 
-// window.addEventListener("hashchange", () => {
-//   navItems.forEach((item) => {
-//     const link = item.querySelector("a").getAttribute("href");
-//     if (link === window.location.hash) {
-//       item.classList.add("active");
-//     } else {
-//       item.classList.remove("active");
-//     }
-//   });
-// });
-
 window.addEventListener("scroll", () => {
   navItems.forEach((item) => {
     const link = item.querySelector("a").getAttribute("href");
@@ -22,4 +11,23 @@ window.addEventListener("scroll", () => {
       item.classList.remove("active");
     }
   });
+});
+
+document.getElementById("btnSwitch").addEventListener("click", () => {
+  const btnSwitch = document.getElementById("btnSwitch");
+  if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+    // if dark -> change light
+    document.documentElement.setAttribute("data-bs-theme", "light");
+    btnSwitch.classList.remove("btn-light");
+    btnSwitch.classList.add("btn-dark");
+    const moonIcon = document.querySelector(".bi-moon-fill");
+    moonIcon.setAttribute("class", "bi bi-moon");
+  } else {
+    // if light -> change dark
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+    btnSwitch.classList.remove("btn-dark");
+    btnSwitch.classList.add("btn-light");
+    const moonIcon = document.querySelector(".bi-moon");
+    moonIcon.setAttribute("class", "bi bi-moon-fill");
+  }
 });
