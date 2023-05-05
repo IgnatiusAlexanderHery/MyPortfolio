18c450d824a1b10f753fc7b216168e168de7a7ea
IgnatiusAlexanderHery <alexander310702@gmail.com>
2023-05-05 22:34:17 +0700

    change theme color, add animation in project image

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ window.addEventListener("scroll", () => {
 
 document.getElementById("btnSwitch").addEventListener("click", () => {
   const btnSwitch = document.getElementById("btnSwitch");
+  const body = document.body;
+  const card = document.querySelectorAll(".card");
+
+  body.style.transition = "1s";
+  card.forEach((card) => {
+    card.style.transition = "1s";
+  });
+
   if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
     // if dark -> change light
     document.documentElement.setAttribute("data-bs-theme", "light");
@@ -30,4 +38,34 @@ document.getElementById("btnSwitch").addEventListener("click", () => {
     const moonIcon = document.querySelector(".bi-moon");
     moonIcon.setAttribute("class", "bi bi-moon-fill");
   }
+});
+
+// Mendapatkan elemen gambar dan modal
+const Image = document.querySelectorAll("img");
+const modal = document.querySelector("#modal");
+const modalImg = document.querySelector("#modalImg");
+const navElements = document.querySelectorAll("nav");
+
+// Menambahkan event listener ke setiap gambar
+Image.forEach(function (Image) {
+  Image.addEventListener("click", function () {
+    modal.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.classList = this.classList;
+    modalImg.style.maxHeight = "80%";
+    modalImg.style.maxWidth = "80%";
+    modalImg.style.margin = "auto";
+    modalImg.style.display = "block";
+    navElements.forEach(function (navElement) {
+      navElement.style.zIndex = "1";
+    });
+  });
+});
+
+// Saat overlay diklik, sembunyikan modal
+modal.addEventListener("click", function () {
+  modal.style.display = "none";
+  navElements.forEach(function (navElement) {
+    navElement.style.zIndex = null;
+  });
 });
