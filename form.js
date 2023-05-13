33d01eeba85881f69ec45a0efa2fa8750a67cc11
IgnatiusAlexanderHery <alexander310702@gmail.com>
2023-05-13 16:31:29 +0700

    add captcha beta

--- form.js
+++ form.js
@@ -1,23 +1,48 @@
 emailjs.init("vg0-LtTdmKj79MQ4p");
 
+function validateForm(event) {
+  event.preventDefault();
+  if (grecaptcha.getResponse() == "") {
+    console.log(grecaptcha.getResponse());
+    return false;
+  } else {
+    console.log(grecaptcha.getResponse() + " else ");
+    grecaptcha.execute();
+    return true;
+  }
+}
+
 document.getElementById("contact-form").addEventListener("submit", function (event) {
   event.preventDefault();
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var message = document.getElementById("pesan").value;
+  if (!validateForm) {
+    console.log("error");
+    alert("Please complete the reCAPTCHA");
+  } else {
+    var sent = document.getElementById("sent");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("pesan").value;
+    sent.disabled = true;
+    var text = sent.innerHTML;
+    sent.innerHTML = "Sending";
+    console.log("sending");
+    var data = {
+      from_name: email + " " + name,
+      to_name: "Alex",
+      message: message,
+    };
 
-  var data = {
-    from_name: email + " " + name,
-    to_name: "Alex",
-    message: message,
-  };
-
-  emailjs.send("service_92iblp8", "template_2tdqgyj", data).then(
-    function (response) {
-      alert("Email berhasil dikirim!");
-    },
-    function (error) {
-      alert("Gagal mengirim email:", error);
-    }
-  );
+    emailjs.send("service_92iblp8", "template_2tdqgyj", data).then(
+      function (response) {
+        alert("Email berhasil dikirim!");
+        document.getElementById("contact-form").reset();
+      },
+      function (error) {
+        alert("Gagal mengirim email:", error);
+      }
+    );
+    sent.innerHTML = text;
+    sentdisabled = false;
+    console.log("sent");
+  }
 });
