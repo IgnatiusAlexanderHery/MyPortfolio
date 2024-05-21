36345807c1dc5c1e9d89e404b0792c39fafae03b
IgnatiusAlexanderHery <alexander310702@gmail.com>
2024-05-21 18:50:19 +0700

    update navigation scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
 import React from "react";
 import { Footer } from "./Components/Footer/Footer";
 import DarkModeButton from "./Utils/DarkMode/DarkModeButton";
-import { useEffect } from "react";
 import { GridContainer } from "./Utils/GridLayout/GridContainer";
 import { Header } from "./Components/Header/Header";
 import getData from "./API/GetData";
 
 function App() {
   const GridDatas = getData();
-
-  useEffect(() => {
-    const navItems = document.querySelectorAll(".nav-item");
-    const handleScroll = () => {
-      navItems.forEach((item) => {
-        const link = item.querySelector("a").getAttribute("href");
-        const target = document.querySelector(link);
-        const pixel = 480;
-        if (target.getBoundingClientRect().top <= pixel && target.getBoundingClientRect().bottom >= pixel) {
-          item.classList.add("font-bold");
-        } else {
-          item.classList.remove("font-bold");
-        }
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
     <div className="App scroll-smooth">
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,37 @@
+import React, { useEffect } from "react";
+
 export const Header = () => {
+  useEffect(() => {
+    const navItems = document.querySelectorAll(".nav-item");
+    const handleScroll = () => {
+      navItems.forEach((item) => {
+        const link = item.querySelector("a").getAttribute("href");
+        const target = document.querySelector(link);
+        const pixel = 480;
+        if (target.getBoundingClientRect().top <= pixel && target.getBoundingClientRect().bottom >= pixel) {
+          item.classList.add("font-bold");
+        } else {
+          item.classList.remove("font-bold");
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function ScrollTo(e) {
+    e.preventDefault();
+    const target = e.target.getAttribute("href")?.replace("#", "");
+    const targetElement = document.getElementById(String(target));
+    window.scrollTo({
+      top: targetElement.offsetTop,
+    });
+  }
+
   return (
     <nav className="navbar shadow-sm bg-blue-400 dark:bg-blue-800 sticky top-0 z-10">
       <div className="flex justify-between p-5">
@@ -6,16 +39,24 @@ export const Header = () => {
         <div className="">
           <ul className="flex justify-evenly gap-4">
             <li className="nav-item">
-              <a href="#Home">Home</a>
+              <a href="#Home" onClick={ScrollTo}>
+                Home
+              </a>
             </li>
             <li className="nav-item">
-              <a href="#About">About</a>
+              <a href="#About" onClick={ScrollTo}>
+                About
+              </a>
             </li>
             <li className="nav-item">
-              <a href="#Projects">Projects</a>
+              <a href="#Projects" onClick={ScrollTo}>
+                Projects
+              </a>
             </li>
             <li className="nav-item">
-              <a href="#Contact">Contact Me</a>
+              <a href="#Contact" onClick={ScrollTo}>
+                Contact Me
+              </a>
             </li>
           </ul>
         </div>
